e5939db18b58d2ee37ee9555006b27209b352d59
Wilker Ribeiro <wilker7ribeiro@gmail.com>
2018-09-20 22:25:56 -0300

    Incluindo heroku

--- src/index.js
+++ src/index.js
@@ -4,50 +4,53 @@ const app = express();
 const bodyParser = require("body-parser");
 const Pessoa = require('./schemas/PessoaSchema')
 const http = require('http');
-  
+
 app.use(bodyParser());
 
 
 const dbName = 'crud-pessoa';
 const dbUser = 'sistema';
-const dbPassword ='admin123'
+const dbPassword = 'admin123'
 mongoose.connect(`mongodb://${dbUser}:${dbPassword}@ds243212.mlab.com:43212/${dbName}`);
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  
-  app.use((req,res,next) => {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-	  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Custom-Header');
-	  next()
-  })
-  
-  app.get('/pessoas', async (req, res, next) => {
-      res.json(await Pessoa.find().exec())
-  })
-  
-  app.get('/pessoa/:id', async (req, res, next) => {
-      res.json(await Pessoa.findById(req.params.id).exec())
-  })
-  
-  app.post('/pessoa', async (req, res, next) => {
-	  const pessoa = new Pessoa(req.body)
-      const saved = await pessoa.save()
-      res.json(saved);
-  })
-  
-  app.put('/pessoa', async (req, res, next) => {
-	  const saved = await Pessoa.update(req.body)
-      res.json(saved);
-  })
-  
-  app.delete('/pessoa/:id', async (req, res, next) => {
-      const saved = await Pessoa.deleteOne({_id: req.params.id})
-      res.json(saved);
-  })
-  
+db.once('open', function () {
 
-  app.listen(8080);
+	app.use((req, res, next) => {
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Custom-Header');
+		next()
+	})
+
+	app.get('/pessoas', async (req, res, next) => {
+		res.json(await Pessoa.find().exec())
+	})
+
+	app.get('/pessoa/:id', async (req, res, next) => {
+		res.json(await Pessoa.findById(req.params.id).exec())
+	})
+
+	app.post('/pessoa', async (req, res, next) => {
+		const pessoa = new Pessoa(req.body)
+		const saved = await pessoa.save()
+		res.json(saved);
+	})
+
+	app.put('/pessoa', async (req, res, next) => {
+		const saved = await Pessoa.update(req.body)
+		res.json(saved);
+	})
+
+	app.delete('/pessoa/:id', async (req, res, next) => {
+		const saved = await Pessoa.deleteOne({ _id: req.params.id })
+		res.json(saved);
+	})
+	let port = process.env.PORT;
+	if (port == null || port == "") {
+		port = 8000;
+	}
+
+	app.listen(port);
 });
